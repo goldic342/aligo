aa6658eb4c58e75e7e5f762540205029f25ab721
goldic <contact@goldic.xyz>
2025-10-24 17:33:15 +0400

    Move data dir creation before sql client connection

--- apps/backend/src/db/client.ts
+++ apps/backend/src/db/client.ts
@@ -1,5 +1,8 @@
 import { SQL } from "bun";
 
+import { mkdir } from "node:fs/promises";
+
+await mkdir("./data/", { recursive: true });
 const sqlite = new SQL({
   adapter: "sqlite",
   filename: "./data/aligo.db",
--- apps/backend/src/db/setup.ts
+++ apps/backend/src/db/setup.ts
@@ -1,8 +1,5 @@
 import { createUserTable } from "../modules/user/model";
 
-import { mkdir } from "node:fs/promises";
-
 export default async function setupTables(): Promise<void> {
-  await mkdir("./data/", { recursive: true });
   await createUserTable();
 }
